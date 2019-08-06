7855b3306ef55e7d1b0141f29f7022f5122959a1
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 17:13:27 +0530

    Create ques10.cpp

---
+++ ques10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int  number1,product;
+    cin>>number1;
+    if(number1>0)
+    {
+        product=number1*2;
+    }
+    cout<<product;
+    return 0;
+}
