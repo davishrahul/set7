92c7df3db9c34b4afbe016356a16541fb91e3d1b
davishrahul <36623942+davishrahul@users.noreply.github.com>
2019-08-06 17:01:28 +0530

    Create ques8.cpp

---
+++ ques8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    if(number%7==0)
+    {
+        cout<<"yes";
+    }   
+    else
+    {
+        cout<<"no";
+    }
+    
+    return 0;
+}
